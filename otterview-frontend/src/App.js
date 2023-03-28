58db8f4ef0835698df395fbdbd74139cbbb9ab43
Isaac Post <ijp5155@rit.edu>
2023-03-28 19:21:59 -0400

    More react setup

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from 'reactstrap';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div class="split left">
-            <div class="centered">
-                <h2>Bye</h2>
-                <p>Some text.</p>
-            </div>
-        </div>
-
-    <div class="split right">
-            <div class="centered">
-                <h2>Hello</h2>
-                <p>Some text here too.</p>
-            </div>
-        </div>
-    </div>
-  );
+class App extends Component {
+  render () {
+        return (
+            <Row>
+                <Col className="black-box">Hello</Col>
+                <Col className="normal-box">Hello</Col>
+            </Row>
+        )
+    }
 }
 
 export default App;
