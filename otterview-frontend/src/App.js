2e02a973d58b9467da27e77dbc5dec6986c92261
Isaac Post <ijp5155@rit.edu>
2023-03-28 19:57:57 -0400

    made TableRow class work

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -4,42 +4,65 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render () {
+
+    renderTableRow(i) {
         return (
-            <div className='scroll-table'>
-                <Table striped bordered>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Twitter Name</th>
-                        <th>Tweet</th>
-                        <th>+ Sentiment</th>
-                        <th>- Sentiment</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>100</td>
-                        <td>-100</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </div>
+            <TableRow
+                id = {i}
+            />
+        )
+    }
+
+
+
+  render () {
+    let html = []
+    for (let i = 1; i <= 100; i++) {
+        html.push(
+            <>{this.renderTableRow(i)}</>
+        );
+    }
+
+    return (
+
+        <div className='scroll-table'>
+            <Table striped bordered>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Twitter Name</th>
+                    <th>Tweet</th>
+                    <th>+ Sentiment</th>
+                    <th>- Sentiment</th>
+                </tr>
+                </thead>
+                <tbody>
+                {html}
+                </tbody>
+            </Table>
+        </div>
+        )
+    }
+}
+
+class TableRow extends Component {
+    render() { 
+        let id = this.props.id;
+        /*
+        let username = this.props.username;
+        let tweet = this.props.tweet;
+        let posSent = this.props.posSent;
+        let negSent = this.props.negSent;
+        */
+
+        return (
+            <tr>
+                <th scope="row">{id}</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>100</td>
+                <td>-100</td>
+            </tr>
         )
     }
 }
