f21d69942a1463d6d854ce6175aaa0784f14b912
Isaac Post <ijp5155@rit.edu>
2023-03-28 19:41:18 -0400

    Setup react table

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -1,15 +1,45 @@
 import './App.css';
-import {Container, Row, Col} from 'reactstrap';
+import {Table} from 'reactstrap';
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render () {
         return (
-            <Row>
-                <Col className="black-box">Hello</Col>
-                <Col className="normal-box">Hello</Col>
-            </Row>
+            <div className='scroll-table'>
+                <Table striped bordered>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Twitter Name</th>
+                        <th>Tweet</th>
+                        <th>+ Sentiment</th>
+                        <th>- Sentiment</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>100</td>
+                        <td>-100</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                    </tbody>
+                </Table>
+            </div>
         )
     }
 }
