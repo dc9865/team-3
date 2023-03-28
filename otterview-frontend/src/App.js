4ea9b86db625488576d2e88e415c9cd4fe8f480b
Isaac Post <ijp5155@rit.edu>
2023-03-28 18:49:28 -0400

    removed react stuff

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. ijp5155
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="split left">
+            <div class="centered">
+                <h2>Bye</h2>
+                <p>Some text.</p>
+            </div>
+        </div>
+
+    <div class="split right">
+            <div class="centered">
+                <h2>Hello</h2>
+                <p>Some text here too.</p>
+            </div>
+        </div>
     </div>
   );
 }
