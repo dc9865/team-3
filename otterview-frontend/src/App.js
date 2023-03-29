e649c0240b8500997c5fe85c7951b00372b70c1b
WhoDaPenguin <61098297+WhoDaPenguin@users.noreply.github.com>
2023-03-29 17:09:50 -0400

    I messed up the build somehow, committing again and hopefully it fixes it

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {Table} from 'reactstrap';
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-# test
 
 class App extends Component {
 
