d0ae8b51ccda2569c2fcd87998c3cf24a8cb0ea2
Isaac Post <ijp5155@rit.edu>
2023-03-29 17:45:55 -0400

    Added mock data to website

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -2,23 +2,36 @@ import './App.css';
 import {Table} from 'reactstrap';
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-# test
 
 class App extends Component {
-
-    renderTableRow(i) {
+    renderTableRow(i, tweetPost) {
         return (
             <TableRow
-                id = {i}
+                id = {i + 1}
+                username = {tweetPost[0]}
+                tweet = {tweetPost[1]}
+                posSent = {tweetPost[2]}
+                negSent = {tweetPost[3]}
             />
         )
     }
 
     render () {
+        const mockData = [["Steve", "No way!", .03, .25],
+                          ["Donalds", "This event was crazy.", .45, .99],
+                          ["Pikachu", "Pika pika!", .01, .01], 
+                          ["Homer", "Mmm... Donuts...", .99, .99],
+                          ["Isaac", "I will never go to this event again!", .02, .78],
+                          ["Gary", "Smell ya later!", .56, .34],
+                          ["Barry", "Im hungry...", .45, .04], 
+                          ["Joe", "Oh no!", 0.01, 0.89],
+                          ["Clara", "I loved it!", 0.13, 0.01], 
+                          ["Mario", "It's me.", 0.56, 0.32]]
+
         let html = []
-        for (let i = 1; i <= 100; i++) {
+        for (let i = 0; i < mockData.length; i++) {
             html.push(
-                <>{this.renderTableRow(i)}</>
+                <>{this.renderTableRow(i, mockData[i])}</>
             );
         }
 
@@ -51,20 +64,18 @@ class App extends Component {
 class TableRow extends Component {
     render() { 
         let id = this.props.id;
-        /*
         let username = this.props.username;
         let tweet = this.props.tweet;
         let posSent = this.props.posSent;
         let negSent = this.props.negSent;
-        */
 
         return (
             <tr>
                 <th scope="row">{id}</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>100</td>
-                <td>-100</td>
+                <td>{username}</td>
+                <td>{tweet}</td>
+                <td>{posSent}</td>
+                <td>{negSent}</td>
             </tr>
         )
     }
