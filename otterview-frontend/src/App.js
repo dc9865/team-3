6a3ee8ff29cefea692d7a01eec2abdbc1df4be7f
Isaac Post <ijp5155@rit.edu>
2023-04-17 17:40:11 -0400

    Uploading but not working

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -6,27 +6,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-circular-progressbar/dist/styles.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={data: []}
+    }
+
+    updateData = (apiResponse) => {
+        this.setState({data: apiResponse})
+    }
+
+    fetchData = () => {
+         fetch('https://ekqyeq1n1k.execute-api.us-east-1.amazonaws.com/test/RetrieveItems/')
+         .then(
+             response => response.json() 
+             )
+         .then (jsonOutput =>
+                  {
+                      this.updateData(jsonOutput)
+                    }
+              )
+    }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+
     renderGradeCircle() {
         return (
             <GradeCircle/>
         )
     }
 
-    renderTableHalf() {
+    renderTableHalf(tweets) {
         return (
-            <TableHalf/>
+            <TableHalf
+                tweets = {tweets}
+            />
         )
     }
 
     render () {
+        let tweets = this.state.data.body
+        
         return (
             <div className='height-setter'>
                 <h1 className='page-head'>OtterView</h1>
                 <h5 className='page-head'>An event sentiment analysis system.</h5>    
 
                 <Row>
-                    {this.renderTableHalf()}
-                    {this.renderGradeCircle()}
+                    {this.renderTableHalf(tweets)}
+                    {this.renderGradeCircle(tweets)}
                 </Row>
             </div>
         )
@@ -46,6 +75,9 @@ class TableHalf extends Component {
     }
 
     render() {
+        // FIX ME
+        let tweets = this.props.tweets;
+
         const mockData = [[1624973346352861184, "RT @MaquinaTipster: Para festejar el #SuperBowl de los chiefs, voy a regalar un jersey de Travis Kelce.  //  // Requisitos para participar  // -Inte…", 0.001331508974544704, 0.2607319951057434],
                           [1624973346235650050, "RT @OffColourOrg: shoutout to ASL interpreter justina miles for absolutely going off for rihanna’s #SuperbOwl performance https://t.co/exVH…", 0.018793117254972458, 0.551336944103241],
                           [1624973345782476803, "RT @jasonselvig: This was hands down the best Super Bowl commercial this year. #SuperBowl https://t.co/Q1BLYyHUy3", 0.003758062608540058, 0.9471458792686462], 
@@ -58,12 +90,8 @@ class TableHalf extends Component {
                           [1624973346042519552, "RT @JJWatt: Script writers in their bag with that one. //  // #SuperBowl", 0.16096551716327667, 0.05783211439847946]]
 
         let html = []
-        for (let i = 0; i < mockData.length; i++) {
-            html.push(
-                <>{this.renderTableRow(mockData[i])}</>
-            );
-        }
-        for (let i = 0; i < mockData.length; i++) {
+
+        for (let i = 0; i < mockData; i++) {
             html.push(
                 <>{this.renderTableRow(mockData[i])}</>
             );
@@ -139,6 +167,9 @@ class GradeCircle extends Component {
     }
 
     render() {
+        // FIX ME
+        let tweets = this.props.tweets;
+
         let percentage = 60
         let numComments = 4500000
 
