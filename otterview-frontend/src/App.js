9815a6cb5c1bec27e7beb9689df1d281557de783
asd1520 <asd1520@rit.edu>
2023-04-17 18:13:03 -0400

    working

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -6,81 +6,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-circular-progressbar/dist/styles.css';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state={data : []}
-    }
-
-    updateData = (apiResponse) => {
-        this.setState({data: apiResponse})
-    }
-
-    fetchData = () => {
-         fetch('https://ekqyeq1n1k.execute-api.us-east-1.amazonaws.com/test/RetrieveItems/')
-         .then(
-             response => response.json() 
-             )
-         .then (jsonOutput =>
-                  {
-                      this.updateData(jsonOutput)
-                    }
-              )
-    }
-
-    componentDidMount(){
-        this.fetchData();
-    }
-
-    /*
     renderGradeCircle() {
         return (
             <GradeCircle/>
         )
     }
 
-    renderTableHalf(tweets) {
+    renderTableHalf() {
         return (
-            <TableHalf
-                tweets = {tweets}
-            />
+            <TableHalf/>
         )
     }
-    */
 
     render () {
-
-        //let tweets = this.state.data
-        
         return (
             <div className='height-setter'>
                 <h1 className='page-head'>OtterView</h1>
                 <h5 className='page-head'>An event sentiment analysis system.</h5>    
 
                 <Row>
-                    {this.state.data}
+                    {this.renderTableHalf()}
+                    {this.renderGradeCircle()}
                 </Row>
             </div>
         )
     }
 }
-/*
+
 class TableHalf extends Component {
     renderTableRow(tweetPost) {
         return (
             <TableRow
-                id = {tweetPost}
-                tweet = {tweetPost}
-                posSent = {tweetPost}
-                negSent = {tweetPost}
+                id = {tweetPost[0]}
+                tweet = {tweetPost[1]}
+                posSent = {tweetPost[2]}
+                negSent = {tweetPost[3]}
             />
         )
     }
 
     render() {
-        // FIX ME
-        let tweets = this.props.tweets;
-
-        
         const mockData = [[1624973346352861184, "RT @MaquinaTipster: Para festejar el #SuperBowl de los chiefs, voy a regalar un jersey de Travis Kelce.  //  // Requisitos para participar  // -Inte…", 0.001331508974544704, 0.2607319951057434],
                           [1624973346235650050, "RT @OffColourOrg: shoutout to ASL interpreter justina miles for absolutely going off for rihanna’s #SuperbOwl performance https://t.co/exVH…", 0.018793117254972458, 0.551336944103241],
                           [1624973345782476803, "RT @jasonselvig: This was hands down the best Super Bowl commercial this year. #SuperBowl https://t.co/Q1BLYyHUy3", 0.003758062608540058, 0.9471458792686462], 
@@ -91,13 +56,16 @@ class TableHalf extends Component {
                           [1624973346281517058, "Congrats to the Chiefs #SuperBowl CHAMPS https://t.co/dU7ER9eHT8", 0.0005397088825702667, 0.9953000545501709],
                           [1624973345836961792, "If Kadarius Toney took that punt return to the house, he probably wins the #SuperBowl MVP.", 0.013535711914300919, 0.2959980368614197], 
                           [1624973346042519552, "RT @JJWatt: Script writers in their bag with that one. //  // #SuperBowl", 0.16096551716327667, 0.05783211439847946]]
-        
-        const mockData = tweets
-        let html = []
 
-        for (let i = 0; i < mockData; i++) {
+        let html = []
+        for (let i = 0; i < mockData.length; i++) {
             html.push(
-                <>{this.renderTableRow(mockData)}</>
+                <>{this.renderTableRow(mockData[i])}</>
+            );
+        }
+        for (let i = 0; i < mockData.length; i++) {
+            html.push(
+                <>{this.renderTableRow(mockData[i])}</>
             );
         }
 
@@ -171,9 +139,6 @@ class GradeCircle extends Component {
     }
 
     render() {
-        // FIX ME
-        let tweets = this.props.tweets;
-
         let percentage = 60
         let numComments = 4500000
 
@@ -201,6 +166,5 @@ class GradeCircle extends Component {
         )
     }
 }
-*/
 
 export default App;
