3f8ccf9765a707897cb1fcb99609987a218c1f9a
asd1520 <asd1520@rit.edu>
2023-04-17 17:43:47 -0400

    not worling

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -78,6 +78,7 @@ class TableHalf extends Component {
         // FIX ME
         let tweets = this.props.tweets;
 
+        /*
         const mockData = [[1624973346352861184, "RT @MaquinaTipster: Para festejar el #SuperBowl de los chiefs, voy a regalar un jersey de Travis Kelce.  //  // Requisitos para participar  // -Inte…", 0.001331508974544704, 0.2607319951057434],
                           [1624973346235650050, "RT @OffColourOrg: shoutout to ASL interpreter justina miles for absolutely going off for rihanna’s #SuperbOwl performance https://t.co/exVH…", 0.018793117254972458, 0.551336944103241],
                           [1624973345782476803, "RT @jasonselvig: This was hands down the best Super Bowl commercial this year. #SuperBowl https://t.co/Q1BLYyHUy3", 0.003758062608540058, 0.9471458792686462], 
@@ -88,7 +89,8 @@ class TableHalf extends Component {
                           [1624973346281517058, "Congrats to the Chiefs #SuperBowl CHAMPS https://t.co/dU7ER9eHT8", 0.0005397088825702667, 0.9953000545501709],
                           [1624973345836961792, "If Kadarius Toney took that punt return to the house, he probably wins the #SuperBowl MVP.", 0.013535711914300919, 0.2959980368614197], 
                           [1624973346042519552, "RT @JJWatt: Script writers in their bag with that one. //  // #SuperBowl", 0.16096551716327667, 0.05783211439847946]]
-
+        */
+        const mockData = tweets
         let html = []
 
         for (let i = 0; i < mockData; i++) {
