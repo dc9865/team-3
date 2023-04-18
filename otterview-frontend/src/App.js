43520e81a23a015c9d7d46f7fa5297886ba1f81d
WhoDaPenguin <61098297+WhoDaPenguin@users.noreply.github.com>
2023-04-18 14:52:56 -0400

    Fixed frontend rendering

--- otterview-frontend/src/App.js
+++ otterview-frontend/src/App.js
@@ -8,7 +8,7 @@ import 'react-circular-progressbar/dist/styles.css';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state={data: []}
+        this.state = {data: []}
     }
 
     updateData = (apiResponse) => {
@@ -16,14 +16,14 @@ class App extends Component {
     }
 
     fetchData = () => {
-         fetch('https://ekqyeq1n1k.execute-api.us-east-1.amazonaws.com/test/RetrieveItems/')
+         fetch('https://aphcuaqzyk.execute-api.us-east-1.amazonaws.com/prod')
          .then(
              response => response.json() 
              )
          .then (jsonOutput =>
-                  {
-                      this.updateData(jsonOutput)
-                    }
+                {
+                    this.updateData(jsonOutput)
+                }
               )
     }
 
@@ -39,7 +39,7 @@ class App extends Component {
 
     renderTableHalf() {
         return (
-            <TableHalf/>
+            <TableHalf data={this.state.data} />
         )
     }
 
@@ -64,30 +64,19 @@ class TableHalf extends Component {
     renderTableRow(tweetPost) {
         return (
             <TableRow
-                id = {tweetPost[0]}
-                tweet = {tweetPost[1]}
-                posSent = {tweetPost[3]}
-                negSent = {tweetPost[2]}
+                id = {tweetPost.id}
+                tweet = {tweetPost.Comment}
+                posSent = {tweetPost.PositiveSentiment}
+                negSent = {tweetPost.NegativeSentiment}
             />
         )
     }
 
     render() {
-        const mockData = [[1624973346352861184, "RT @MaquinaTipster: Para festejar el #SuperBowl de los chiefs, voy a regalar un jersey de Travis Kelce.  //  // Requisitos para participar  // -Inte…", 0.001331508974544704, 0.2607319951057434],
-                          [1624973346235650050, "RT @OffColourOrg: shoutout to ASL interpreter justina miles for absolutely going off for rihanna’s #SuperbOwl performance https://t.co/exVH…", 0.018793117254972458, 0.551336944103241],
-                          [1624973345782476803, "RT @jasonselvig: This was hands down the best Super Bowl commercial this year. #SuperBowl https://t.co/Q1BLYyHUy3", 0.003758062608540058, 0.9471458792686462], 
-                          [1624973346114007040, "RT @dearestbelcalis: LMFAO Cardi B and Offset running to make sure they don’t miss Rihanna’s halftime performance is sending meeeeee 😭😭😭😭 #…", 0.029037199914455414, 0.18745966255664825],
-                          [1624973345786630147, "hEr sUrPriSe gUesT wAs heR fuggin baby broooooooo #WHAT #WOW #SuperBowl https://t.co/zwSWlYTJb3", 0.2792544364929199, 0.18727873265743256],
-                          [1624973345706967048, "RT @beyoncebrasil: AQUI ELA FOI GRANDONA: Cantou “Formation” um dia depois de lançar a música, fez referência aos Panteras Negras, incomodo…", 0.010629395954310894, 0.8310677409172058],
-                          [1624973346159964160, "RT @choquei: 🚨BRASIL: Remix de “Rude Boy” feito por Rihanna no #SuperBowl  foi feito por DJ Klean, de 20 anos, da Bahia. https://t.co/rBrCv…", 0.000059077076002722606, 0.00344928284175694], 
-                          [1624973346281517058, "Congrats to the Chiefs #SuperBowl CHAMPS https://t.co/dU7ER9eHT8", 0.0005397088825702667, 0.9953000545501709],
-                          [1624973345836961792, "If Kadarius Toney took that punt return to the house, he probably wins the #SuperBowl MVP.", 0.013535711914300919, 0.2959980368614197], 
-                          [1624973346042519552, "RT @JJWatt: Script writers in their bag with that one. //  // #SuperBowl", 0.16096551716327667, 0.05783211439847946]]
-
         let html = []
-        for (let i = 0; i < mockData.length; i++) {
+        for (let i = 0; i < this.props.data.length; i++) {
             html.push(
-                <>{this.renderTableRow(mockData[i])}</>
+                <>{this.renderTableRow(this.props.data[i])}</>
             );
         }
 
